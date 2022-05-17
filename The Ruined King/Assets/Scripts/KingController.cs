b00781283596b43aab92cbac8f6b0f20497b43b6
Perez <maxchanceperez@gmail.com>
2022-05-17 09:44:53 -0700

    have jumping

--- The Ruined King/Assets/Scripts/KingController.cs
+++ The Ruined King/Assets/Scripts/KingController.cs
@@ -4,55 +4,16 @@ using UnityEngine;
 
 public class KingController : MonoBehaviour
 {
-    public float speed = 3.0f;
-    public float jumpForce;
-    public float gravityModifier;
-    float horizontal;
-    float vertical;
-
-    public bool isOnGround = true;
-    Rigidbody2D rigidbody2d;
-    Vector2 lookDirection = new Vector2(1, 0);
+    
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        Physics2D.gravity *= gravityModifier;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
-
-        if (Input.GetButtonDown("up") && isOnGround)
-        {
-            rigidbody2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isOnGround = false;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
-
-        rigidbody2d.MovePosition(position);
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        isOnGround = true;
+        
     }
 }
--- The Ruined King/Assets/Scripts/PlayerMovement.cs
+++ The Ruined King/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
 
     float horizontalMove = 0f;
+    public float runSpeed = 40f;
 
+    bool jump = false;
     void Start()
     {
         
@@ -16,11 +18,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal");
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
