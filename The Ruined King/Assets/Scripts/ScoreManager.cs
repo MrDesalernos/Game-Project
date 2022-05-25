a934401e493935d71d79e6159b5208195a0e502b
Foolish-Cherry <duranfabian1204@gmail.com>
2022-05-25 08:41:06 -0700

    Started Working on Score

---
+++ The Ruined King/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public Text scoreText;
+    public Text highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = score.ToString() + " Score";
+        highscoreText.text = "Highscore: " + highscore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
