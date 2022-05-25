eb4bcdc643f725f5cb963181e2cf53e1d796381b
MrDesalernos <maxchanceperez@gmail.com>
2022-05-24 21:30:39 -0700

    Level created

--- The Ruined King/Assets/Scripts/StopWatch.cs
+++ The Ruined King/Assets/Scripts/StopWatch.cs
@@ -6,7 +6,7 @@ public class StopWatch : MonoBehaviour
 {
     bool stopwatchActive = false;
     float currentTime;
-    public Text currentTimeText;
+    //public Text currentTimeText;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +21,8 @@ public class StopWatch : MonoBehaviour
         {
             currentTime = currentTime - Time.deltaTime;
         }
-        TimeSpan time = TimeSpan.FromSeconds(currentTime);
-        currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+        //TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        //currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
     }
 
     public void StartStopwatch()
